ce8d1087397e6c48c2a27eada48ba249b008e2c7
Vladimir Bekker <vladimir.bekker1337@gmail.com>
2026-07-23 14:43:09 +0200

    feat: fill contribution entities

--- core-service/src/main/kotlin/com/umt/core/contribution/Contributor.kt
+++ core-service/src/main/kotlin/com/umt/core/contribution/Contributor.kt
@@ -1,4 +1,40 @@
 package com.umt.core.contribution
 
-class Contributor {
+import jakarta.persistence.*
+import org.hibernate.annotations.JdbcTypeCode
+import org.hibernate.type.SqlTypes
+import java.util.UUID
+
+enum class ContributorType { PERSON, ORGANIZATION }
+
+@Entity
+@Table(name = Contributor.TABLE_NAME)
+class Contributor(
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @field:Column(name = ID_COLUMN)
+    var id: UUID? = null,
+
+    @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    @field:Column(name = CONTRIBUTOR_TYPE_COLUMN, nullable = false)
+    var contributorType: ContributorType,
+
+    @field:Column(name = NAME_COLUMN, nullable = false, length = 255)
+    var name: String,
+
+    @field:Column(name = DESCRIPTION_COLUMN)
+    var description: String? = null,
+
+    @field:Column(name = IMAGE_URL_COLUMN)
+    var imageUrl: String? = null,
+) {
+    companion object {
+        const val TABLE_NAME = "contributor"
+        const val ID_COLUMN = "id"
+        const val CONTRIBUTOR_TYPE_COLUMN = "contributor_type"
+        const val NAME_COLUMN = "name"
+        const val DESCRIPTION_COLUMN = "description"
+        const val IMAGE_URL_COLUMN = "image_url"
+    }
 }
--- core-service/src/main/kotlin/com/umt/core/contribution/Credit.kt
+++ core-service/src/main/kotlin/com/umt/core/contribution/Credit.kt
@@ -1,4 +1,39 @@
 package com.umt.core.contribution
 
-class Credit {
+import com.umt.core.media.MediaItem
+import jakarta.persistence.*
+import org.hibernate.annotations.JdbcTypeCode
+import org.hibernate.type.SqlTypes
+import java.util.UUID
+
+enum class RoleType { DIRECTOR, DEVELOPER, AUTHOR, ARTIST, WRITER, STUDIO, PUBLISHER }
+
+@Entity
+@Table(name = Credit.TABLE_NAME)
+class Credit(
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @field:Column(name = ID_COLUMN)
+    var id: UUID? = null,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = MEDIA_ITEM_ID_COLUMN, nullable = false)
+    var mediaItem: MediaItem,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = CONTRIBUTOR_ID_COLUMN, nullable = false)
+    var contributor: Contributor,
+
+    @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    @field:Column(name = ROLE_COLUMN, nullable = false)
+    var role: RoleType,
+) {
+    companion object {
+        const val TABLE_NAME = "credit"
+        const val ID_COLUMN = "id"
+        const val MEDIA_ITEM_ID_COLUMN = "media_item_id"
+        const val CONTRIBUTOR_ID_COLUMN = "contributor_id"
+        const val ROLE_COLUMN = "role"
+    }
 }
